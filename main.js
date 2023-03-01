786558ddc00d923e8359ada26565cdb241e531a6
Rihanul Islam <rihanthedev@gmail.com>
2023-03-01 20:04:11 +0600

    header done

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ const showCategories = (data)=>{
   // console.log(data);
   const categoriesContainer = document.getElementById('categories-container')
   data.news_category.forEach(singleCategory => {
-    categoriesContainer.innerHTML+=`<a class="nav-link" href="#">${singleCategory.category_name}</a>`
+    categoriesContainer.innerHTML+=`<a class="nav-link" href="#" onclick="fetchCategoriesAllNews('${singleCategory.category_id}')">${singleCategory.category_name}</a>`
   });
+}
+// fetch the all available News
+const fetchCategoriesAllNews = category_id =>{
+  // console.log(category_id);
+  const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`
+  fetch(url).then(res=>res.json()).then(data=>console.log(data))
 }

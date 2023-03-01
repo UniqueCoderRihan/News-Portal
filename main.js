9f94600a6b3a52ef5e2b3155cdb2aae404407e4f
Rihanul Islam <rihanthedev@gmail.com>
2023-03-01 19:39:44 +0600

    categoty-added-

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const fetchCategories = ()=>{
   fetch('https://openapi.programming-hero.com/api/news/categories')
   .then(res=>res.json())
-  .then(data=>console.log(data))
+  .then(data=>showCategories(data.data))
 }
 
-
-
-
+const showCategories = (data)=>{
+  // console.log(data);
+  const categoriesContainer = document.getElementById('categories-container')
+  data.news_category.forEach(singleCategory => {
+    categoriesContainer.innerHTML+=`<a class="nav-link" href="#">${singleCategory.category_name}</a>`
+  });
+}

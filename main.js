3057b1962afdec66b95457848e713c568e12359b
Rihanul Islam <rihanthedev@gmail.com>
2023-03-01 20:22:14 +0600

    alert section added

--- main.js
+++ main.js
@@ -8,12 +8,18 @@ const showCategories = (data)=>{
   // console.log(data);
   const categoriesContainer = document.getElementById('categories-container')
   data.news_category.forEach(singleCategory => {
-    categoriesContainer.innerHTML+=`<a class="nav-link" href="#" onclick="fetchCategoriesAllNews('${singleCategory.category_id}')">${singleCategory.category_name}</a>`
+    categoriesContainer.innerHTML+=`<a class="nav-link" href="#" onclick="fetchCategoriesAllNews('${singleCategory.category_id}','${singleCategory.category_name}')">${singleCategory.category_name}</a>`
   });
 }
 // fetch the all available News
-const fetchCategoriesAllNews = category_id =>{
-  // console.log(category_id);
+const fetchCategoriesAllNews = (category_id,category_name) =>{
+  // console.log(category_id,category_name);
   const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`
-  fetch(url).then(res=>res.json()).then(data=>console.log(data))
+  fetch(url).then(res=>res.json()).then(data=>showAllNews(data.data,category_name))
+}
+
+const showAllNews = (data,category_name)=>{
+  console.log(data,category_name);
+  document.getElementById('news_count').innerText = data.length;
+  document.getElementById('catagory_name').innerText = category_name;
 }
